93d2fc3663238c33dd08da86f9c7fa2cc77a93fc
goyalion17 <goyalion17@gmail.com>
2019-09-03 20:07:59 +0300

    first react project

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div className="List">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default List;
